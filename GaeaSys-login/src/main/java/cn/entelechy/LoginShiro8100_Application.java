aba537ca97724a86a88498615d4527f41a2a28b8
Entelechy-xiao <yingtelaixi@163.com>
2019-07-19 13:25:11 +0800

    盖亚监管系统1.0

---
+++ GaeaSys-login/src/main/java/cn/entelechy/LoginShiro8100_Application.java
@@ -0,0 +1,11 @@
+package cn.entelechy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginShiro8100_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(LoginShiro8100_Application.class,args);
+    }
+}
---
+++ GaeaSys-login/src/main/java/cn/entelechy/configbeans/ShiroConfiguration.java
@@ -0,0 +1,8 @@
+package cn.entelechy.configbeans;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfiguration {
+
+}
