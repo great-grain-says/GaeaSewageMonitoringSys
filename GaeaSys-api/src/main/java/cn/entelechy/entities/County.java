8dac54a0823900374744e39e7171659d0afb3c7c
Entelechy-xiao <yingtelaixi@163.com>
2019-07-29 09:35:18 +0800

    新增工厂数据采集模块

--- GaeaSys-api/src/main/java/cn/entelechy/entities/County.java
+++ GaeaSys-api/src/main/java/cn/entelechy/entities/County.java
@@ -8,6 +8,7 @@ import lombok.experimental.Accessors;
 public class County {
     private Integer county_id;
     private String county_name;
+    private Integer city_id;
 
     public Integer getCounty_id() {
         return county_id;
@@ -23,5 +24,13 @@ public class County {
 
     public void setCounty_name(String county_name) {
         this.county_name = county_name;
+    }
+
+    public Integer getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(Integer city_id) {
+        this.city_id = city_id;
     }
 }
